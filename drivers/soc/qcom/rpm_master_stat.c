b6a737459a42fda562237bb54d914e2aad2dcf66
Nathan Chancellor <natechancellor@gmail.com>
2019-04-23 17:46:07 -0700

    soc: qcom: rpm_master_stat: Fix strlcpy usage
    Clang warns:
    
    ../drivers/soc/qcom/rpm_master_stat.c:400:13: warning: size argument in
    'strlcpy' call appears to be size of the source; expected the size of
    the destination [-Wstrlcpy-strlcat-size]
                                            strlen(master_name) + 1);
                                            ~~~~~~~^~~~~~~~~~~~~~~~
    
    62a79b84fd85 ("qcom: Port rpm logs and stats driver from 3.18 to 4.4 kernel")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/soc/qcom/rpm_master_stat.c
+++ drivers/soc/qcom/rpm_master_stat.c
@@ -395,9 +395,9 @@ static struct msm_rpm_master_stats_platform_data
 				strlen(master_name) + 1, GFP_KERNEL);
 		if (!pdata->masters[i])
 			goto err;
-		rpm_master_name_len = strlen(master_name);
-		strlcpy(pdata->masters[i], master_name,
-					rpm_master_name_len + 1);
+
+		strscpy(pdata->masters[i], master_name,
+					sizeof(pdata->masters[i]));
 	}
 	return pdata;
 err:
