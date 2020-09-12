4398043fd048d53c66fe70374c1a676301429977
build <2243168976@qq.com>
2020-09-13 02:50:35 +0800

    drivers: qcedev: Fix build

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1781,15 +1781,8 @@ long qcedev_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-<<<<<<< HEAD
-					sizeof(struct qcedev_sha_op_req))) {
-			err = -EFAULT;
-			goto exit_free_qcedev_areq;
-		}
-=======
-					sizeof(struct qcedev_sha_op_req)))
+        			sizeof(struct qcedev_sha_op_req)))
 			return -EFAULT;
->>>>>>> 6c79848769f0... crypto: msm: update QTI crypto drivers for msm-4.14
 		handle->sha_ctxt.init_done = false;
 		break;
 
@@ -1834,15 +1827,8 @@ long qcedev_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 				handle->sha_ctxt.diglen);
 		mutex_unlock(&hash_access_lock);
 		if (copy_to_user((void __user *)arg, &qcedev_areq->sha_op_req,
-<<<<<<< HEAD
-					sizeof(struct qcedev_sha_op_req))) {
-			err = -EFAULT;
-			goto exit_free_qcedev_areq;
-			}
-=======
 					sizeof(struct qcedev_sha_op_req)))
 			return -EFAULT;
->>>>>>> 6c79848769f0... crypto: msm: update QTI crypto drivers for msm-4.14
 		}
 		break;
 	default:
